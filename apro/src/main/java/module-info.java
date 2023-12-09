d1f26b8041e6a9607773d45cf1e32b197717c23f
yash-2587 <133660162+yash-2587@users.noreply.github.com>
2023-12-09 18:00:41 +0530

    Delete apro directory

--- apro/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.apro {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-    opens com.example.apro to javafx.fxml;
-    exports com.example.apro;
-}
